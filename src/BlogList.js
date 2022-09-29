c5376b657abefe02c734a6324f373091014a1e9f
Ivan Chan Yin Man <scskchan@gmail.com>
2022-09-29 21:59:30 +0800

    fix url

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = ( { blogs, title } ) => {
             { blogs.map( blog => (  
                 <div className="blog-preview" key={ blog.id } > 
 
-                <Link to={ `/blogs/${ blog.id }` }>                
+                <Link to={ `/blogs/${ blog._id }` }>                
                     <h2> { blog.title } </h2>
                     <p> Written by { blog.author } </p>
                 </Link>
